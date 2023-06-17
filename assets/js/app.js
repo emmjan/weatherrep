e58f4ed8358aa47471c08234738edaac699660f5
Emma Margaryan <margaryem@gmail.com>
2023-06-17 11:51:51 +0200

    added serviceworker

--- assets/js/app.js
+++ assets/js/app.js
@@ -74,3 +74,13 @@ let searchPosition = (position) => {
     cityInput.value = `${position.coords.latitude}, ${position.coords.longitude}`
     weatherForm.requestSubmit()
 }
+
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker
+            .register("serviceWorker.js")
+            .then(res => console.log("service worker registered"))
+            .catch(err => console.log("service worker not registered", err))
+    })
+}
